c48bf71b538e1c22295caf52fec4a911d03076b3
Tony <legendmastertony@gmail.com>
2025-03-12 11:55:35 +0800

    Add crate level docs

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,33 @@
+//! # Tauri Runtime Verso
+//!
+//! Use Verso as the backend for Tauri
+//!
+//! ## Usage
+//!
+//! ```
+//! use tauri::Manager;
+//! use tauri_runtime_verso::{set_verso_path, set_verso_resource_directory, INVOKE_SYSTEM_SCRIPTS};
+//!
+//! fn main() {
+//!     // You need to set this to the path of the versoview executable
+//!     // before creating any of the webview windows
+//!     set_verso_path("../verso/target/debug/versoview");
+//!     // Set this to verso/servo's resources directory before creating any of the webview windows
+//!     // this is optional but recommanded, this directory will include very important things
+//!     // like user agent stylesheet
+//!     set_verso_resource_directory("../verso/resources");
+//!     tauri::Builder::<tauri_runtime_verso::VersoRuntime>::new()
+//!         // Make sure to do this or some of the commands will not work
+//!         .invoke_system(INVOKE_SYSTEM_SCRIPTS.to_owned())
+//!         .run(tauri::generate_context!())
+//!         .unwrap();
+//! }
+//! ```
+//!
+//! ## Cargo features
+//!
+//! - **macos-private-api**: Matching with Tauri's macos-private-api feature, required if you use that
+
 #![allow(unused)]
 
 use tauri::{LogicalPosition, LogicalSize};
